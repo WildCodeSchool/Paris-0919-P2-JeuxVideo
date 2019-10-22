52acd6672e914e311108a4cdcbeb280d4f6a9f5a
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-22 17:36:45 +0200

    added flexbox properties to elements

--- src/Components/GameSynopsis.js
+++ src/Components/GameSynopsis.js
@@ -5,13 +5,16 @@ import './GameSynopsis.css'
 class GameSynopsis extends React.Component {
     render() {
         return (
-            <div >
+            <div className ="synopsis_parent" >
                 <h2 class='title_synopsis'> Le jeu </h2>
-                <p className='text_synopsis'> King.DOM Quest est un jeu de rôle dans l’univers du code.
+                <div className='text_synopsis'> King.DOM Quest est un jeu de rôle dans l’univers du code.
                   Le gameplay est inspiré par les classiques japonais des années 80/90 (Dragon Quest, Final Fantasy…). Vous incarnez l’élu, le seul espoir pour sauver le King.DOM.
                   Armé de votre fidèle keyBoardBlade(), parcourez le royaume de King.DOM pour le sauver des Bugs.
-                </p>
-                <button class='supportButton'>Soutenir le jeu</button>
+                </div>
+                <div className="supportButtonParent">
+                    <button class='supportButton'>Soutenir le jeu</button>
+                </div>
+                
             </div>
         );
     }
