801bb6467ebfa92691e5a7fd2e7daa9b06d65cd1
Steven <steven.pigere@gmail.com>
2019-10-22 11:31:36 +0200

    Ecran de jeu V1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/Header'
+import Ecran from './Components/Ecran'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Ecran />
     </div>
   );
 }
