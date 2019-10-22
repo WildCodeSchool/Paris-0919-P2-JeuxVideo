2ddbde1f5d454c84058be556a5a2fc68e6883eab
Steven <steven.pigere@gmail.com>
2019-10-22 14:47:46 +0200

    Partie Kickstarter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './Components/Header'
 import Ecran from './Components/Ecran'
+import Kickstarter from './Components/Kickstarter'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Ecran />
+      <Kickstarter />
     </div>
   );
 }
