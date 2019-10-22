d351949c85a9daf2f763125048e94503e5c67828
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-22 15:56:35 +0200

    added value to the form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -3,21 +3,32 @@ import './ContactForm.css'
 
 class ContactForm extends React.Component {
 
+    state = {
+        email : 'mail',
+        inquiry : 'inquiry'
+        message : 'Text...'
+
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form className='contactForm'>
                     <input
                         id='email'
                         type='text'
+                        value= {this.state.email}
                     />
                     <input
                         id='reason'
                         type='text'
+                        value={this.state.value}
+
                     />
                     <input
                         id='message'
                         type='text'
+                        value={this.state.message}
                     />
                 </form>
 
