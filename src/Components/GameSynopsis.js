64ae4088cafa086838547c91c0e5de7c636f36d5
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-22 21:11:42 +0200

    added margin between elements

--- src/Components/GameSynopsis.js
+++ src/Components/GameSynopsis.js
@@ -6,9 +6,11 @@ class GameSynopsis extends React.Component {
     render() {
         return (
             <div className ="synopsis_parent" >
-                <h2 class='title_synopsis'> Le jeu </h2>
+                <h2 className='title_synopsis'> Le jeu </h2>
                 <div className='text_synopsis'> King.DOM Quest est un jeu de rôle dans l’univers du code.
-                  Le gameplay est inspiré par les classiques japonais des années 80/90 (Dragon Quest, Final Fantasy…). Vous incarnez l’élu, le seul espoir pour sauver le King.DOM.
+                  Le gameplay est inspiré par les classiques japonais des années 80/90 (Dragon Quest, Final Fantasy…).
+                </div>
+                <div className='text_synopsis'> Vous incarnez l’élu, le seul espoir pour sauver le King.DOM.
                   Armé de votre fidèle keyBoardBlade(), parcourez le royaume de King.DOM pour le sauver des Bugs.
                 </div>
                 <div className="supportButtonParent">
