6f1a7ae2b778a680e21f23fa0ed7c40983c517e0
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-22 14:47:07 +0200

    Added js + css files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './Components/Header'
 import Ecran from './Components/Ecran'
+import ContactForm from './Components/ContactForm'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Ecran />
+      <ContactForm/>
     </div>
   );
 }
---
+++ src/Components/ContactForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ContactForm.css'
+
+class ContactForm extends React.Component {
+    render () {
+        return(
+            <div>
+                <h2>Contact us</h2>
+            </div>
+        )
+    }
+    
+}
+
+
+export default ContactForm;
