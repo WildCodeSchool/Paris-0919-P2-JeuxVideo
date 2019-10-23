0a45dae3fc3241d9d489e8c8180a4caf1d16d484
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-23 13:54:28 +0200

    creation of footer component + css

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <div></div>
+        )
+    }    
+}
+
+
+export default Footer;
+
