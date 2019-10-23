421a89539db0ffcd9bf2bd1bb85b5dd5e7dc8974
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-23 16:06:04 +0200

    fixed the style and the flex

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,22 +6,22 @@ import LogoInsta from './instagram-logo.svg';
 
 
 class Footer extends React.Component {
-    render () {
+    render() {
         return (
             <div >
-                <footer className = 'footerTest' >
-                    <p className ='mailStudio'>ragegitstudio@ragegit.com</p>
-                    <nav className ='navFooter'>
-                        <ul className ='listeFooter'>
-                            <li className ='elmtSns'><img className ='snsImg' src={LogoInsta} alt='logo instagram' /></li>
-                            <li className ='elmtSns'><img className ='snsImg' src={LogoFb} alt='logo facebook' /></li>
-                            <li className ='elmtSns'><img className ='snsImg' src={LogoTwitter} alt='logo twitter' /></li>
-                        </ul>
-                    </nav>
+                <footer className='footerTest' >
+                    <p className='mailStudio'>ragegitstudio@ragegit.com</p>
+
+                    <ul className='listeFooter'>
+                        <li className='elmtSns'><img className='snsImg' src={LogoInsta} alt='logo instagram' /></li>
+                        <li className='elmtSns'><img className='snsImg' src={LogoFb} alt='logo facebook' /></li>
+                        <li className='elmtSns'><img className='snsImg' src={LogoTwitter} alt='logo twitter' /></li>
+                    </ul>
+
                 </footer>
             </div>
         )
-    }    
+    }
 }
 
 
