f8185260f4aa2fa03bc6a862288fc20e73c92d3f
Tass2T <brunog90@gmail.com>
2019-10-28 15:25:11 +0100

    game-area in place

---
+++ map/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import GameManager from './Components/Game-manager'
+
+function App() {
+  return (
+    <div className="App">
+      <GameManager/>
+    </div>
+  );
+}
+
+export default App;
---
+++ map/src/Components/Game-manager.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./Game-manager.css"
+
+class GameManager extends React.Component{
+
+    render(){
+        return(
+            <div className ="Game-area">
+            </div>
+        )
+    }
+}
+
+export default GameManager
