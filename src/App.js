68794ef554465401d7bdba1d395c84386861c0b0
Nelo <kevinpplle@gmail.com>
2019-11-04 13:36:22 +0100

    Hp decrease

--- src/App.js
+++ src/App.js
@@ -7,56 +7,56 @@ import Command from './components/Commands'
 import Dialog from './components/Dialog'
 import './App.css';
 
-  
+
 class App extends React.Component {
   state = {
     HP: 150
   }
   newHPClickedChild = neoClickedHP => {
     this.setState({
-    HP: neoClickedHP
-     })
-     this.props.newHPEnemyClicked(neoClickedHP);
-    };
+      HP: neoClickedHP
+    })
+
+  };
 
   render() {
     console.log(this.state.HP)
     return (
       <div className="App">
-       <div className='game-area'>
-         
-         <div className='enemystatus-area'>
-         <Command 
-          newHPClicked={this.newHPClickedChild}
-           />
-         <div className="meta-area">
-         <Enemy name={'Meta'}
-         newHPEnemyClicked={this.EnemyClicked}/>
-            <img className='Meta' src={Meta} alt='Meta'></img>
-            
-          </div>
-         </div>
-         <div className='playerstatus-area'>
-           <div className='avatar-area'>
-         <img className='avatar' src={Avatar} alt='Avatar'></img>
-         <Player/>
-         </div>
-            <div className='dialog-area'>
-            
-            <Dialog/>
-            
+        <div className='game-area'>
+
+          <div className='enemystatus-area'>
+            <Command
+              newHPClicked={this.newHPClickedChild}
+            />
+            <div className="meta-area">
+              <Enemy name={'Meta'}/>
+                {this.state.HP}
+              <img className='Meta' src={Meta} alt='Meta'></img>
+
             </div>
-            
-            
-            
-            
-         </div>
-       </div>
+          </div>
+          <div className='playerstatus-area'>
+            <div className='avatar-area'>
+              <img className='avatar' src={Avatar} alt='Avatar'></img>
+              <Player />
+            </div>
+            <div className='dialog-area'>
+
+              <Dialog />
+
+            </div>
+
+
+
+
+          </div>
+        </div>
       </div>
     );
 
   }
-  
+
 }
 
 export default App;
--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -7,17 +7,17 @@ class Enemy extends React.Component {
         HP: 150,
     }
 
-EnemyClicked = EnemyHP => {
-        this.setState({
-        HP: EnemyHP
-         })}
+// EnemyClicked = EnemyHP => {
+//         this.setState({
+//         HP: EnemyHP
+//          })}
 
 render() {
     return (
         <>
         <div className="enemy-area">
             <h2>{this.props.name}</h2>
-            <p>HP : {this.state.HP}</p>
+            
         </div>
         </>
     )
