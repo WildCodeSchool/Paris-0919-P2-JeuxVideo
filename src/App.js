6140b4b1f9bba3c51ea6b3818ace80760eac5343
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-11-06 14:59:03 +0100

    merged api with current branch test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import './App.css';
-
+import Axios from 'axios'
 import GameManager from './Components/Game-manager'
 
-function App() {
-  return (
-    <div className="App">
-      <GameManager/>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+  // Initialize states
+  state = {
+    textureDatas:'',
+    itemsDatas: '',
+    soundsDatas: '',
+    charactersDatas: ''
+  }
+
+  // Use Axios to consume APIs
+  componentDidMount() {
+
+    // Texture API
+    Axios.get('./database/textures.json')
+      // Change JSON into JS object
+      .then(response => response.data)
+      // Give the texture object to the state
+      .then(data => {
+        this.setState({ textureDatas: data })
+      })
+
+    // Item API
+    Axios.get('./database/items.json')
+    // Change JSON into JS object
+    .then(response => response.data)
+    // Give the texture object to the state
+    .then(data => {
+      this.setState({ itemsDatas: data })
+    })
+
+    // Sound API
+    Axios.get('./database/characters.json')
+    // Change JSON into JS object
+    .then(response => response.data)
+    // Give the texture object to the state
+    .then(data => {
+      this.setState({ soundsDatas: data })
+    })
+
+    // Characters API
+    Axios.get('./database/sounds.json')
+    // Change JSON into JS object
+    .then(response => response.data)
+    // Give the texture object to the state
+    .then(data => {
+      this.setState({ charactersDatas: data })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <GameManager/>
+      </div>
+    );
+  }
 }
 
-export default App;
