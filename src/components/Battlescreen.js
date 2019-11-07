1e78a587b2c0bf4431f1328efa5a410caf781040
Barbara Paul <barbara.kikou@gmail.com>
2019-11-07 17:56:44 +0100

    added a function so thta the enemy attacks the player

--- src/components/Battlescreen.js
+++ src/components/Battlescreen.js
@@ -12,17 +12,25 @@ import Popup from './Popup';
 
 
 class Battlescreen extends React.Component {
+
   state = {
     HP: 150,
     dialog: 'A wild meta appears',
     isDead: false,
     showPopup: false,
+    HpPlayer: 200,
   }
+
   newHPClickedChild = neoClickedHP => {
     this.setState({
       HP: neoClickedHP,
     })
   };
+
+ 
+
+
+
   // Ici, notre méthode pour actualiser la boite de dialogue et les HP.
   handleDamage = () => {
     if (this.state.HP < 0) {
@@ -33,7 +41,24 @@ class Battlescreen extends React.Component {
         showPopup: true,
 
       })
+    } 
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.HP !== this.state.HP) {
+      setTimeout( () =>
+        this.enemyAttack(),        
+        3000
+      )
     }
+  }
+  
+  enemyAttack = () => {
+      const newhpPlayer = this.state.HpPlayer- (Math.floor(Math.random() * 50));
+      this.setState({        
+        HpPlayer: newhpPlayer,
+        dialog: "The meta attacked you"
+      }) 
   }
 
   handlePop = (isHere) =>{
@@ -50,22 +75,18 @@ class Battlescreen extends React.Component {
             {this.state.showPopup ? (<Popup newHPClicked={this.newHPClickedChild} />) : (console.log('nothing'))}
             <div className="meta-area">
               <Enemy name={'Meta'} HP={this.state.HP} onChange={this.handleDamage()} />
- 
-
-
-              <img className='Meta' src={this.state.isDead ? MetaDead : Meta} alt='Meta'></img>
-
+              <img className='Meta' src={this.state.isDead ? MetaDead : Meta} alt='Meta'/>
             </div>
           </div>
           <div className='playerstatus-area'>
             <div className='avatar-area'>
               <img className='avatar' src={Avatar} alt='Avatar'></img>
-              <Player />
+              <Player  HpPlayer={this.state.HpPlayer} />
             </div>
             <div className='dialog-area'>
 
 
-              <Dialog dialog={this.state.dialog} onChange={this.handleDamage()} />
+              <Dialog dialog={this.state.dialog} onChange={ this.handleDamage()} />
 
 
             </div>
--- src/components/Player.js
+++ src/components/Player.js
@@ -3,16 +3,14 @@ import React from 'react';
 import './Player.css';
 
 class Player extends React.Component {
-    state = {
-        HP: 200
-    }
+
 
 
 render() {
     return (
         <div className="player-area">
             <h2>Avatar</h2>
-            <p>HP : {this.state.HP}</p>
+            <p>HP : {this.props.HpPlayer}</p>
         </div>
     )
 }
