6b4e100eb77ecf88265d9a82f3225f7a9d2e72a8
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-11-14 16:41:21 +0100

    created the GameOver component + linked the img in the map.json

---
+++ src/Components/GameOver.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GameOver extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>Thank you for playing the demo of King.DOM Quest ! </h2>
+                <img src='' alt = ''/>
+                <p>Unfortunately, today the king.DOM is still not safe from bugs... unless ? </p>
+                <input value ='Start a new game'></input>
+
+            </div>
+        )
+    }
+}
+
+export default GameOver;
