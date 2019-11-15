73406efdc002fc2d5e7dde3e70c63667cdba0609
Tass2T <brunog90@gmail.com>
2019-11-15 09:03:46 +0100

    block before battle

--- src/Components/Map3.js
+++ src/Components/Map3.js
@@ -45,6 +45,7 @@ class Map3 extends React.Component {
 
     spawnBattle = () => {
         if (this.dice === 1) {
+            this.blockDeplacement = 1
             document.querySelector('.map_background').style.backgroundImage = ''
             document.querySelector('.map_background').style.animation = "flash 0.65s"
             document.querySelector('.abdou').style.display = 'none'
@@ -58,6 +59,7 @@ class Map3 extends React.Component {
     }
 
     blockCombat = 0
+    blockDeplacement = 0
 
     // Move the character, change its direction & animation
     onKeyDown = (e) => {
@@ -65,16 +67,16 @@ class Map3 extends React.Component {
         switch (e.keyCode) {
             case 90: //up
             case 38:
-                if (this.state.top <= 1 && this.state.left <= 9 && this.state.left >= 6) { //here to block the shortcut
+                if (this.state.top <= 1 && this.state.left <= 9 && this.blockDeplacement===0 && this.state.left >= 6) { //here to block the shortcut
                     this.props.newTop(7)
                     this.props.newLeft(this.state.left - 1)
                     this.props.newMap(4)
                 }
                 else {
-                    if (this.state.position !== 'top 100px right 300px' && !this.state.lockMovement) { 
+                    if (this.state.position !== 'top 100px right 300px' && this.blockDeplacement===0 && !this.state.lockMovement) { 
                         this.setState({ position: 'top 100px right 300px' })
                     }
-                    else if (this.state.top > 1 && !this.state.lockMovement && this.state.map[this.state.top - 2][this.state.left - 1] === 0) {
+                    else if (this.state.top > 1 && !this.state.lockMovement && this.blockDeplacement===0 && this.state.map[this.state.top - 2][this.state.left - 1] === 0) {
                         this.setState({position : 'top 100px right 400px', top : this.state.top-1})
                         if (this.blockCombat < 4){
                             this.blockCombat += 1
@@ -87,10 +89,10 @@ class Map3 extends React.Component {
                 break
             case 83: //down
             case 40:
-                if (this.state.position !== 'top 400px right 400px' && !this.state.lockMovement) {
+                if (this.state.position !== 'top 400px right 400px' && this.blockDeplacement===0 && !this.state.lockMovement) {
                     this.setState({ position: 'top 400px right 400px' })
                 }
-                else if (this.state.top < 7 && !this.state.lockMovement && this.state.map[this.state.top][this.state.left - 1] === 0) { 
+                else if (this.state.top < 7 && this.blockDeplacement===0 && !this.state.lockMovement && this.state.map[this.state.top][this.state.left - 1] === 0) { 
                     const down = this.state.top + 1
                     this.setState({ position: 'top 400px right 300px', top: down })
                     if (this.blockCombat < 4){
@@ -110,10 +112,10 @@ class Map3 extends React.Component {
                     this.props.newTop(this.state.top)
                     this.props.newMap(2)
                 } else {
-                    if (this.state.position !== 'top 300px right 300px' && !this.state.lockMovement) { 
+                    if (this.state.position !== 'top 300px right 300px' && this.blockDeplacement===0 && !this.state.lockMovement) { 
                         this.setState({ position: 'top 300px right 300px' })
                     }
-                    else if (this.state.left > 1 && !this.state.lockMovement && this.state.map[this.state.top - 1][this.state.left - 2] === 0) {
+                    else if (this.state.left > 1 && this.blockDeplacement===0 && !this.state.lockMovement && this.state.map[this.state.top - 1][this.state.left - 2] === 0) {
                         const left = this.state.left - 1
                         this.setState({ position: 'top 300px right 400px', left: left })
                         if (this.blockCombat < 4){
@@ -129,10 +131,10 @@ class Map3 extends React.Component {
                 break
             case 68: // right
             case 39:
-                if (this.state.position !== 'top 200px right 300px' && !this.state.lockMovement) {
+                if (this.state.position !== 'top 200px right 300px' && this.blockDeplacement===0 && !this.state.lockMovement) {
                     this.setState({ position: 'top 200px right 300px', })
                 }
-                else if (this.state.left < 14 && !this.state.lockMovement && (this.state.map[this.state.top - 1][this.state.left] === 0 || this.state.map[this.state.top - 1][this.state.left] === undefined)) {
+                else if (this.state.left < 14 && this.blockDeplacement===0 && !this.state.lockMovement && (this.state.map[this.state.top - 1][this.state.left] === 0 || this.state.map[this.state.top - 1][this.state.left] === undefined)) {
                     const right = this.state.left + 1
                     this.setState({ left: right })
                     if (this.blockCombat < 4){
